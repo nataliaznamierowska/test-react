f57182a0dd94b20f7504cdd40a6cc247629400de
natalia_znamierowska <nat.znamierowska@gmail..com>
2023-09-05 21:33:02 +0200

    changes

--- src/components/About/Employee.jsx
+++ src/components/About/Employee.jsx
@@ -1,12 +1,12 @@
-import manPicture from "../../images/man.jpg";
-import womanPicture from "../../images/woman.jpg";
+import firstWomanPicture from "../../images/pracownik-1.png";
+import womanPicture from "../../images/pracownik-2.jpg";
 
 const Employee = ({ id, firstName, lastName, position, description }) => {
   return (
     <div className="employee">
       <div className="employee-photo-wrapper">
         <img
-          src={id === 1 ? manPicture : womanPicture}
+          src={id === 1 ? firstWomanPicturePicture : womanPicture}
           alt="Zdjęcie pracownika"
           className="employee-photo"
         />
